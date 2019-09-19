7777bc175ca89bd59f34c151751040799cd07612
Scala Steward <me@scala-steward.org>
2019-09-19 07:44:07 +0200

    Update scala-logging to 3.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.1.0"
-  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 }
