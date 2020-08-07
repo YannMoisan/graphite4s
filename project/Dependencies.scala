2d4770f130a58aad420db53758620d0ee4749756
Scala Steward <me@scala-steward.org>
2020-08-07 09:55:17 +0200

    Update scalatest to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.1"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.0.0"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 }
