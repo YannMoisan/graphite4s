a7097139789c844d7d75347c078be590dbc2b4e6
Scala Steward <me@scala-steward.org>
2022-03-25 05:16:15 +0100

    Update cats-effect-kernel to 3.3.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest    = "org.scalatest"              %% "scalatest"          % "3.2.11"
-  lazy val catsEffect   = "org.typelevel"              %% "cats-effect-kernel" % "3.3.5"
+  lazy val catsEffect   = "org.typelevel"              %% "cats-effect-kernel" % "3.3.9"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.4"
 }
