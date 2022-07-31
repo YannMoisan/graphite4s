3ae1c14116346d45d650459bd0c7bc47ddd30507
Scala Steward <scala_steward@virtuslab.com>
2022-07-31 19:39:06 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest    = "org.scalatest"              %% "scalatest"          % "3.2.11"
+  lazy val scalaTest    = "org.scalatest"              %% "scalatest"          % "3.2.13"
   lazy val catsEffect   = "org.typelevel"              %% "cats-effect-kernel" % "3.3.5"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.4"
 }
