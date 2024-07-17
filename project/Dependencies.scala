94d5d03e4eb4b356a98f2c8f101da9340d54a25a
Yann Moisan <yann@zen.ly>
2024-07-18 00:37:09 +0200

    bump cats-effect to 3.4.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest        = "org.scalatest"              %% "scalatest"          % "3.2.15"
-  lazy val catsEffectKernel = "org.typelevel"              %% "cats-effect-kernel" % "3.4.6"
-  lazy val catsEffect       = "org.typelevel"              %% "cats-effect"        % "3.4.6"
+  lazy val catsEffectKernel = "org.typelevel"              %% "cats-effect-kernel" % "3.4.11"
+  lazy val catsEffect       = "org.typelevel"              %% "cats-effect"        % "3.4.11"
   lazy val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.5"
 }
